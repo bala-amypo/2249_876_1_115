d666197d0cfe4d16947c81fc5792420bdfc01d8c
bala-amypo <bala@amypo.in>
2025-12-19 09:53:12 +0000

    User commit

--- src/main/java/com/example/demo/entity/ShipmentRecord.java
+++ src/main/java/com/example/demo/entity/ShipmentRecord.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 @Entity
+@Data
 @Getter
 @Setter
 public class ShipmentRecord {
--- src/main/java/com/example/demo/entity/TemperatureRule.java
+++ src/main/java/com/example/demo/entity/TemperatureRule.java
@@ -7,6 +7,7 @@ import lombok.Setter;
 import java.time.LocalDateTime;
 
 @Entity
+@D
 @Getter
 @Setter
 public class TemperatureRule {
