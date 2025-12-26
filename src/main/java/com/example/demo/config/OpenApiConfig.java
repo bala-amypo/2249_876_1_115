dbdb02886c748d60c00fce7b43591d5a9ca641eb
bala-amypo <bala@amypo.in>
2025-12-26 04:03:21 +0000

    User commit

--- src/main/java/com/example/demo/config/OpenApiConfig.java
+++ src/main/java/com/example/demo/config/OpenApiConfig.java
@@ -1,20 +1,27 @@
 package com.example.demo.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 import java.util.List;
 
 @Configuration
-public class OpenApiConfig {
+public class SwaggerConfig {
 
     @Bean
-    public OpenAPI customOpenAPI() {
+    public OpenAPI openAPI() {
         return new OpenAPI()
-                // You need to change the port as per your server
+                .info(new Info()
+                        .title("Cold Chain Temperature Breach Alert System")
+                        .version("1.0")
+                        .description("Swagger API documentation for Cold Chain System"))
                 .servers(List.of(
-                        new Server().url("")
+                        new Server().url("http://localhost:9001")
                 ));
-        }
+    }
+}
+
 }
